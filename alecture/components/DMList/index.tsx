8a93e2cd90066f0770cc73a876b5174d8cb2b0c5
chesedi <a02t02@gmail.com>
2021-07-09 19:43:26 +0900

    add infinity scroll

--- alecture/components/DMList/index.tsx
+++ alecture/components/DMList/index.tsx
@@ -1,163 +1,89 @@
-import ChatBox from '@components/ChatBox';
-import ChatList from '@components/ChatList';
-import useInput from '@hooks/useInput';
+// import useSocket from '@hooks/useSocket';
+import { CollapseButton } from '@components/DMList/styles';
 import useSocket from '@hooks/useSocket';
-import { Container, Header, DragOver } from '@pages/DirectMessage/styles';
-import { IDM } from '@typings/db';
+import { IUser, IUserWithOnline } from '@typings/db';
 import fetcher from '@utils/fetcher';
-import makeSection from '@utils/makeSection';
-import axios from 'axios';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import gravatar from 'gravatar';
-import Scrollbars from 'react-custom-scrollbars';
+import React, { FC, useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import useSWR, { useSWRInfinite } from 'swr';
+import { NavLink } from 'react-router-dom';
+import useSWR from 'swr';
 
-const DirectMessage = () => {
-  const { workspace, id } = useParams<{ workspace: string; id: string }>();
-  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
-  const { data: myData } = useSWR('/api/users', fetcher);
-  const [chat, onChangeChat, setChat] = useInput('');
+const DMList: FC = () => {
+  const { workspace } = useParams<{ workspace?: string }>();
   const {
-    data: chatData,
-    mutate: mutateChat,
+    data: userData,
+    error,
     revalidate,
-    setSize,
-  } = useSWRInfinite<IDM[]>(
-    (index) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=${index + 1}`,
+    mutate,
+  } = useSWR<IUser>('/api/users', fetcher, {
+    dedupingInterval: 2000, // 2초
+  });
+  const { data: memberData } = useSWR<IUserWithOnline[]>(
+    userData ? `/api/workspaces/${workspace}/members` : null,
     fetcher,
   );
   const [socket] = useSocket(workspace);
-  const isEmpty = chatData?.[0]?.length === 0;
-  const isReachingEnd = isEmpty || (chatData && chatData[chatData.length - 1]?.length < 20) || false;
-  const scrollbarRef = useRef<Scrollbars>(null);
-  const [dragOver, setDragOver] = useState(false);
+  const [channelCollapse, setChannelCollapse] = useState(false);
+  const [onlineList, setOnlineList] = useState<number[]>([]);
 
-  const onSubmitForm = useCallback(
-    (e) => {
-      e.preventDefault();
-      console.log(chat);
-      if (chat?.trim() && chatData) {
-        const savedChat = chat;
-        mutateChat((prevChatData) => {
-          prevChatData?.[0].unshift({
-            id: (chatData[0][0]?.id || 0) + 1,
-            content: savedChat,
-            SenderId: myData.id,
-            Sender: myData,
-            ReceiverId: userData.id,
-            Receiver: userData,
-            createdAt: new Date(),
-          });
-          return prevChatData;
-        }, false).then(() => {
-          setChat('');
-          scrollbarRef.current?.scrollToBottom();
-        });
-        axios
-          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
-            content: chat,
-          })
-          .then(() => {
-            revalidate();
-          })
-          .catch(console.error);
-      }
-    },
-    [chat, chatData, myData, userData, workspace, id],
-  );
-
-  const onMessage = useCallback((data: IDM) => {
-    // id는 상대방 아이디
-    if (data.SenderId === Number(id) && myData.id !== Number(id)) {
-      mutateChat((chatData) => {
-        chatData?.[0].unshift(data);
-        return chatData;
-      }, false).then(() => {
-        if (scrollbarRef.current) {
-          if (
-            scrollbarRef.current.getScrollHeight() <
-            scrollbarRef.current.getClientHeight() + scrollbarRef.current.getScrollTop() + 150
-          ) {
-            console.log('scrollToBottom!', scrollbarRef.current?.getValues());
-            setTimeout(() => {
-              scrollbarRef.current?.scrollToBottom();
-            }, 50);
-          }
-        }
-      });
-    }
+  const toggleChannelCollapse = useCallback(() => {
+    setChannelCollapse((prev) => !prev);
   }, []);
 
   useEffect(() => {
-    socket?.on('dm', onMessage);
+    console.log('DMList: workspace 바꼈다', workspace);
+    setOnlineList([]);
+  }, [workspace]);
+
+  useEffect(() => {
+    socket?.on('onlineList', (data: number[]) => {
+      setOnlineList(data);
+    });
+    // socket?.on('dm', onMessage);
+    // console.log('socket on dm', socket?.hasListeners('dm'), socket);
     return () => {
-      socket?.off('dm', onMessage);
+      // socket?.off('dm', onMessage);
+      // console.log('socket off dm', socket?.hasListeners('dm'));
+      socket?.off('onlineList');
     };
-  }, [socket, onMessage]);
-
-  // 로딩 시 스크롤바 제일 아래로
-  useEffect(() => {
-    if (chatData?.length === 1) {
-      setTimeout(() => {
-        scrollbarRef.current?.scrollToBottom();
-      }, 100);
-    }
-  }, [chatData]);
-
-  const onDrop = useCallback(
-    (e) => {
-      e.preventDefault();
-      console.log(e);
-      const formData = new FormData();
-      if (e.dataTransfer.items) {
-        // Use DataTransferItemList interface to access the file(s)
-        for (let i = 0; i < e.dataTransfer.items.length; i++) {
-          // If dropped items aren't files, reject them
-          if (e.dataTransfer.items[i].kind === 'file') {
-            const file = e.dataTransfer.items[i].getAsFile();
-            console.log('... file[' + i + '].name = ' + file.name);
-            formData.append('image', file);
-          }
-        }
-      } else {
-        // Use DataTransfer interface to access the file(s)
-        for (let i = 0; i < e.dataTransfer.files.length; i++) {
-          console.log('... file[' + i + '].name = ' + e.dataTransfer.files[i].name);
-          formData.append('image', e.dataTransfer.files[i]);
-        }
-      }
-      axios.post(`/api/workspaces/${workspace}/dms/${id}/images`, formData).then(() => {
-        setDragOver(false);
-        revalidate();
-      });
-    },
-    [revalidate, workspace, id],
-  );
-
-  const onDragOver = useCallback((e) => {
-    e.preventDefault();
-    console.log(e);
-    setDragOver(true);
-  }, []);
-
-  if (!userData || !myData) {
-    return null;
-  }
-
-  const chatSections = makeSection(chatData ? chatData.flat().reverse() : []);
+  }, [socket]);
 
   return (
-    <Container onDrop={onDrop} onDragOver={onDragOver}>
-      <Header>
-        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
-        <span>{userData.nickname}</span>
-      </Header>
-      <ChatList chatSections={chatSections} ref={scrollbarRef} setSize={setSize} isReachingEnd={isReachingEnd} />
-      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
-      {dragOver && <DragOver>업로드!</DragOver>}
-    </Container>
+    <>
+      <h2>
+        <CollapseButton collapse={channelCollapse} onClick={toggleChannelCollapse}>
+          <i
+            className="c-icon p-channel_sidebar__section_heading_expand p-channel_sidebar__section_heading_expand--show_more_feature c-icon--caret-right c-icon--inherit c-icon--inline"
+            data-qa="channel-section-collapse"
+            aria-hidden="true"
+          />
+        </CollapseButton>
+        <span>Direct Messages</span>
+      </h2>
+      <div>
+        {!channelCollapse &&
+          memberData?.map((member) => {
+            const isOnline = onlineList.includes(member.id);
+            return (
+              <NavLink key={member.id} activeClassName="selected" to={`/workspace/${workspace}/dm/${member.id}`}>
+                <i
+                  className={`c-icon p-channel_sidebar__presence_icon p-channel_sidebar__presence_icon--dim_enabled c-presence ${
+                    isOnline ? 'c-presence--active c-icon--presence-online' : 'c-icon--presence-offline'
+                  }`}
+                  aria-hidden="true"
+                  data-qa="presence_indicator"
+                  data-qa-presence-self="false"
+                  data-qa-presence-active="false"
+                  data-qa-presence-dnd="false"
+                />
+                <span>{member.nickname}</span>
+                {member.id === userData?.id && <span> (나)</span>}
+              </NavLink>
+            );
+          })}
+      </div>
+    </>
   );
 };
 
-export default DirectMessage;
+export default DMList;
--- alecture/pages/DirectMessage/index.tsx
+++ alecture/pages/DirectMessage/index.tsx
@@ -1,17 +1,14 @@
-// import ChatBox from '@components/ChatBox';
-// import ChatList from '@components/ChatList';
-// import useSocket from '@hooks/useSocket';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useSocket from '@hooks/useSocket';
 import { Container, Header, DragOver } from '@pages/DirectMessage/styles';
-// import { IDM } from '@typings/db';
-// import makeSection from '@utils/makeSection';
-// import axios from 'axios';
+import makeSection from '@utils/makeSection';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import gravatar from 'gravatar';
-// import Scrollbars from 'react-custom-scrollbars';
+import Scrollbars from 'react-custom-scrollbars';
 import { useParams } from 'react-router';
 import useSWR, { useSWRInfinite } from 'swr';
 import fetcher from '@utils/fetcher';
-import ChatBox from '@components/ChatBox';
 import useInput from '@hooks/useInput';
 import axios from 'axios';
 import { IDM } from '@typings/db';
@@ -30,33 +27,33 @@ const DirectMessage = () => {
     (index) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=${index + 1}`,
     fetcher,
   );
-  // const [socket] = useSocket(workspace);
-  // const isEmpty = chatData?.[0]?.length === 0;
-  // const isReachingEnd = isEmpty || (chatData && chatData[chatData.length - 1]?.length < 20) || false;
-  // const scrollbarRef = useRef<Scrollbars>(null);
+  const [socket] = useSocket(workspace);
+  const isEmpty = chatData?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (chatData && chatData[chatData.length - 1]?.length < 20) || false;
+  const scrollbarRef = useRef<Scrollbars>(null);
   // const [dragOver, setDragOver] = useState(false);
 
   const onSubmitForm = useCallback(
     (e) => {
       e.preventDefault();
       console.log(chat);
-      if (chat?.trim()) {
-        // const savedChat = chat;
-        // mutateChat((prevChatData) => {
-        //   prevChatData?.[0].unshift({
-        //     id: (chatData[0][0]?.id || 0) + 1,
-        //     content: savedChat,
-        //     SenderId: myData.id,
-        //     Sender: myData,
-        //     ReceiverId: userData.id,
-        //     Receiver: userData,
-        //     createdAt: new Date(),
-        //   });
-        //   return prevChatData;
-        // }, false).then(() => {
-        //   setChat('');
-        //   scrollbarRef.current?.scrollToBottom();
-        // });
+      if (chat?.trim() && chatData) {
+        const savedChat = chat;
+        mutateChat((prevChatData) => {
+          prevChatData?.[0].unshift({
+            id: (chatData[0][0]?.id || 0) + 1,
+            content: savedChat,
+            SenderId: myData.id,
+            Sender: myData,
+            ReceiverId: userData.id,
+            Receiver: userData,
+            createdAt: new Date(),
+          });
+          return prevChatData;
+        }, false).then(() => {
+          setChat('');
+          scrollbarRef.current?.scrollToBottom();
+        });
         axios
           .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
             content: chat,
@@ -70,43 +67,43 @@ const DirectMessage = () => {
     [chat, chatData, myData, userData, workspace, id],
   );
 
-  // const onMessage = useCallback((data: IDM) => {
-  //   // id는 상대방 아이디
-  //   if (data.SenderId === Number(id) && myData.id !== Number(id)) {
-  //     mutateChat((chatData) => {
-  //       chatData?.[0].unshift(data);
-  //       return chatData;
-  //     }, false).then(() => {
-  //       if (scrollbarRef.current) {
-  //         if (
-  //           scrollbarRef.current.getScrollHeight() <
-  //           scrollbarRef.current.getClientHeight() + scrollbarRef.current.getScrollTop() + 150
-  //         ) {
-  //           console.log('scrollToBottom!', scrollbarRef.current?.getValues());
-  //           setTimeout(() => {
-  //             scrollbarRef.current?.scrollToBottom();
-  //           }, 50);
-  //         }
-  //       }
-  //     });
-  //   }
-  // }, []);
+  const onMessage = useCallback((data: IDM) => {
+    // id는 상대방 아이디
+    if (data.SenderId === Number(id) && myData.id !== Number(id)) {
+      mutateChat((chatData) => {
+        chatData?.[0].unshift(data);
+        return chatData;
+      }, false).then(() => {
+        if (scrollbarRef.current) {
+          if (
+            scrollbarRef.current.getScrollHeight() <
+            scrollbarRef.current.getClientHeight() + scrollbarRef.current.getScrollTop() + 150
+          ) {
+            console.log('scrollToBottom!', scrollbarRef.current?.getValues());
+            setTimeout(() => {
+              scrollbarRef.current?.scrollToBottom();
+            }, 50);
+          }
+        }
+      });
+    }
+  }, []);
 
-  // useEffect(() => {
-  //   socket?.on('dm', onMessage);
-  //   return () => {
-  //     socket?.off('dm', onMessage);
-  //   };
-  // }, [socket, onMessage]);
+  useEffect(() => {
+    socket?.on('dm', onMessage);
+    return () => {
+      socket?.off('dm', onMessage);
+    };
+  }, [socket, onMessage]);
 
-  // // 로딩 시 스크롤바 제일 아래로
-  // useEffect(() => {
-  //   if (chatData?.length === 1) {
-  //     setTimeout(() => {
-  //       scrollbarRef.current?.scrollToBottom();
-  //     }, 100);
-  //   }
-  // }, [chatData]);
+  // 로딩 시 스크롤바 제일 아래로
+  useEffect(() => {
+    if (chatData?.length === 1) {
+      setTimeout(() => {
+        scrollbarRef.current?.scrollToBottom();
+      }, 100);
+    }
+  }, [chatData]);
 
   // const onDrop = useCallback(
   //   (e) => {
@@ -148,7 +145,7 @@ const DirectMessage = () => {
     return null;
   }
 
-  // const chatSections = makeSection(chatData ? chatData.flat().reverse() : []);
+  const chatSections = makeSection(chatData ? chatData.flat().reverse() : []);
 
   return (
     <Container>
@@ -156,7 +153,7 @@ const DirectMessage = () => {
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      {/* <ChatList chatSections={chatSections} ref={scrollbarRef} setSize={setSize} isReachingEnd={isReachingEnd} /> */}
+      <ChatList chatSections={chatSections} ref={scrollbarRef} setSize={setSize} isReachingEnd={isReachingEnd} />
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
       {/* {dragOver && <DragOver>업로드!</DragOver>} */}
     </Container>
